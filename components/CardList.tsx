78cc3cbc05656d3b9b27824f3c656937506ad022
Halyna Stepanenko <halyna.stepanenko@lidingo.se>
2023-09-12 17:57:38 +0200

    feat: add gsap branch

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { List, ListItem, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ModalCard from "./ModalCard";
-
+import gsap from "gsap";
+import { CSSPlugin, ScrollTrigger } from "gsap/all";
 export interface SearchResponse {
   id: number;
   first_name: string;
@@ -19,6 +20,9 @@ export default function CardList({ inputValue }: CardListProps) {
   const [jsonData, setJsonData] = useState<SearchResponse[] | null>(null);
   const [cards, setCards] = useState<SearchResponse[] | null>(null);
   const [openModalId, setOpenModalId] = React.useState<number | null>(null);
+  const itemEls = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
 
   const handleModalOpen = (id: number) => setOpenModalId(id);
   const handleModalClose = (e: React.MouseEvent) => {
@@ -51,22 +55,54 @@ export default function CardList({ inputValue }: CardListProps) {
             obj.first_name.toLowerCase().includes(inputValue) ||
             obj.last_name.toLowerCase().includes(inputValue)
         );
-
         setCards(filteredData);
       }
     }
   }, [inputValue]);
 
+  //gsap animation
+  useEffect(() => {
+    if (cards !== null) {
+      const items = document.querySelectorAll(".card");
+      const tl = gsap.timeline();
+
+      items.forEach((el, index) => {
+        gsap.fromTo(
+          el,
+          {
+            duration: 0.1,
+            opacity: 0,
+            force3D: true,
+            //stagger: index * 0.01,
+            delay: index * 0.01,
+          },
+          { opacity: 1 }
+        );
+        gsap.fromTo(
+          el,
+          {
+            duration: 0.1,
+            scale: index % 2 === 0.8 ? 0 : 1.2,
+            //delay: index * 0.01,
+          },
+          { scale: 1 }
+        );
+        //return () => tl.kill();
+      });
+    }
+  }, [cards]);
+
   return (
     inputValue !== "" &&
     cards !== null && (
       <List
+        ref={itemEls}
         sx={{
           mb: { xs: 5, sm: 0 },
         }}
         className="flex flex-wrap gap-5 justify-center"
       >
-        {cards.map((obj) => (
+        {cards.map((obj, index) => (
           <ListItem
             key={obj.id}
             onClick={() => handleModalOpen(obj.id)}
